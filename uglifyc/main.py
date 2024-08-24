e7070a21784725bba910c048b1e9f9869dfa89ed
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2024-08-24 15:30:51 +0200

    v0.1.3 Fixed argument naming mismatch

--- uglifyc/main.py
+++ uglifyc/main.py
@@ -3,40 +3,38 @@ import click
 def get_max_width(file):
     max_width = 0
     with open(file, "r") as f:
-        for i in f.readlines():
-            if len(i) > max_width:
-                max_width = len(i)
-
+        for line in f:
+            if len(line) > max_width:
+                max_width = len(line)
     return max_width
 
 def get_first_normal_char_from_right(s, char_list, space_number):
     for index in range(len(s) - 1, -1, -1):
         if s[index] not in char_list:
-            return s[:index+1 ] + ' '*space_number + s[index+1:]
+            return s[:index+1] + ' '*space_number + s[index+1:]
     return s
 
 @click.command()
 @click.argument('input_file', type=click.Path(exists=True))
-@click.option('-o', '--output', type=click.Path(), help='Output file for the uglified Python code.')
-def uglify(input, output):
+@click.option('-o', '--output', type=click.Path(), help='Output file for the uglified code.')
+def uglify(input_file, output):
     """
     Uglifies a C file.
     """
     # Set the output file name
     if output is None:
-        output = input.replace('.c', '_uglified.c')
+        output = input_file.replace('.c', '_uglified.c')
 
     char_list = ["}", ";", ")", "\n", " ", "{"]
 
-    max_width = get_max_width(input)
+    max_width = get_max_width(input_file)
 
     uglified_code = ""
 
-    with open(input, "r") as f:
-        for line in f.readlines():
+    with open(input_file, "r") as f:
+        for line in f:
             space_number = max_width - len(line)
-
-            uglified_code += get_first_normal_char_from_right(line, char_list, space_number+10)
+            uglified_code += get_first_normal_char_from_right(line, char_list, space_number + 10)
 
     with open(output, 'w') as outfile:
         outfile.write(uglified_code)
